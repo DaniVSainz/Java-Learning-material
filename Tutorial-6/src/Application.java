e6289b050e9867eae1c0c252317553877231bd1c
DaniVSainz <danivsainz@gmail.com>
2017-05-23 10:19:39 -0400

    Tut

--- Tutorial-6/src/Application.java
+++ Tutorial-6/src/Application.java
@@ -1,9 +1,23 @@
 
 public class Application {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int myInt = 20;
+		
+		if(myInt < 2) {
+			System.out.println("something");
+		}
+		else if(myInt > 2){
+			System.out.println("Larger than 2");
+		}else{
+			System.out.println("No its false ");
+		}
+		
+		int loop = 0;
+		while(loop < 5){
+			System.out.println("Looping" + loop);
+			loop++;
+		}
 	}
 
 }
