c6c96e792e5a034ddf63e215666dc790356b6bb9
DaniVSainz <danivsainz@gmail.com>
2017-05-23 09:50:34 -0400

    more java tutorial

---
+++ Tutorial-3/src/Application.java
@@ -0,0 +1,23 @@
+
+public class Application {
+	public static void main(String[] args) {
+		int myInt = 7;
+		
+		String text = "hello";
+		
+		String blank = " ";
+		
+		String name = "Daniel";
+
+		String greeting = text + blank + name; 
+
+		System.out.println(greeting);
+
+		System.out.println("Bob" + " " + " Something Somethingson");
+
+		System.out.println("My integer is " + myInt);
+
+		double doubleInt = 7.8;
+		System.out.println();
+	}
+}
