5afdde5058404f689abb7dd3445c1f59c1f51938
DaniVSainz <danivsainz@gmail.com>
2017-05-25 11:35:06 -0400

    code for encapsulation

---
+++ encapsulation/src/encapsulation/App.java
@@ -0,0 +1,34 @@
+package encapsulation;
+
+class Plant{
+	public static final int ID = 7;
+	private String name;
+
+	public String getData(){
+		String something = "Some data and CalculatedGrowth = " + calculateGrowthForecast();
+		return something;
+	}
+	
+	private int calculateGrowthForecast(){
+		return 9;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
